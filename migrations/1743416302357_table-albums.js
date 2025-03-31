9e13f5c31597d06ea87f8a9572d6f35a982432b2
csamsul2017 <csamsul2017@gmail.com>
2025-03-31 17:22:26 +0700

    split schema table in file migrations

--- migrations/1743416302357_table-albums.js
+++ migrations/1743416302357_table-albums.js
@@ -1,7 +1,6 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-exports.shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
@@ -23,37 +22,6 @@ exports.up = (pgm) => {
       notNull: true,
     },
   });
-
-  pgm.createTable('songs', {
-    id: {
-      type: 'VARCHAR(50)',
-      primaryKey: true,
-    },
-    title: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    year: {
-      type: 'SMALLINT',
-      notNull: true,
-    },
-    genre: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    performer: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    duration: {
-      type: 'INTEGER',
-    },
-    albumId: {
-      type: 'VARCHAR(50)',
-      references: 'albums(id)',
-      onDelete: 'CASCADE',
-    },
-  });
 };
 
 /**
@@ -62,6 +30,5 @@ exports.up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
-  pgm.dropTable('songs');
   pgm.dropTable('albums');
 };
